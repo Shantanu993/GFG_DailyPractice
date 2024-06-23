212ed984349e23987bca9fc56cf99fb1191a0d52
Shantanu Gahtori <shantanugahtori4@gmail.com>
2024-06-23 10:31:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Binary Search/binary-search.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+  public class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int key = sc.nextInt();
+            Solution g = new Solution();
+            System.out.println(g.binarysearch(arr, n, key));
+            T--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int lp=0;
+        int up=n-1;
+        int p;
+        
+        while(lp<=up){
+            
+            p=(lp+up)/2;
+            
+            if(arr[p]==k){
+                
+                return p;
+            }
+            
+            if(arr[p]>k){
+                
+                up=p-1;
+            }
+            
+            if(arr[p]<k){
+                
+                lp=p+1;
+            }
+            
+        }
+        
+        return -1;
+    }
+}

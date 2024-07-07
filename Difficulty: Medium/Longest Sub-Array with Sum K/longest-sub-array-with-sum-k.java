fbdc3f42d43b585cb1d7bbfcaa81047f1972bd66
Shantanu Gahtori <shantanugahtori4@gmail.com>
2024-07-07 23:28:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+   
+    // Function for finding maximum and value pair
+    public static int lenOfLongSubarr (int A[], int N, int K) {
+        //Complete the function
+        int maxlen=0;
+        int sum=0;
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        for(int i=0;i<A.length;i++){
+            sum+=A[i];
+            if(sum==K){
+                maxlen=Math.max(maxlen,i+1);
+            }
+            
+            if(mp.containsKey(sum-K)){
+                maxlen=Math.max(maxlen,i-mp.get(sum-K));
+            }
+            if(!mp.containsKey(sum)){
+                mp.put(sum,i);
+            }
+        }
+        return maxlen;
+    }
+    
+    
+}

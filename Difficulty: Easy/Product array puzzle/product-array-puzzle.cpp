99ddff220c5d428e798b256c97369affe9dfda5b
Shantanu Gahtori <shantanugahtori4@gmail.com>
2024-08-17 22:50:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
@@ -1,66 +1,59 @@
 //{ Driver Code Starts
-//Initial template for C++
+// Initial template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
-
 // } Driver Code Ends
-//User function template for C++
+// User function template for C++
 
 
 class Solution {
-public:
-vector<long long> productExceptSelf(vector<long long>& nums, int n) {
-    vector<long long> left(n, 1);
-    vector<long long> right(n, 1);
-    vector<long long> result(n, 1);
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+   vector<long long int> productExceptSelf(vector<long long int>& nums) {
+    int n = nums.size();
+    vector<long long int> ans(n);
 
-    // Fill left array
-    for (int i = 1; i < n; i++) {
-        left[i] = left[i - 1] * nums[i - 1];
-    }
-
-    // Fill right array
-    for (int i = n - 2; i >= 0; i--) {
-        right[i] = right[i + 1] * nums[i + 1];
-    }
-
-    // Fill result array
     for (int i = 0; i < n; i++) {
-        result[i] = left[i] * right[i];
+        long long int pro=1;
+        for (int j = 0; j < n; j++) {
+            if (i != j) {
+                pro *= nums[j];
+            }
+        }
+        ans[i]=pro;
     }
-
-    return result;
+    return ans;
 }
+
 };
 
 
 //{ Driver Code Starts.
-int main()
- {
-    int t;  // number of test cases
-    cin>>t;
-    while(t--)
-    {
-        int n;  // size of the array
-        cin>>n;
-        vector<long long int> arr(n),vec(n);
-        
-        for(int i=0;i<n;i++)    // input the array
+int main() {
+    int t; // number of test cases
+    cin >> t;
+    while (t--) {
+        int n; // size of the array
+        cin >> n;
+        vector<long long int> arr(n), vec(n);
+
+        for (int i = 0; i < n; i++) // input the array
         {
-            cin>>arr[i];
+            cin >> arr[i];
         }
         Solution obj;
-        vec = obj.productExceptSelf(arr,n);   // function call
-        
-        for(int i=0;i<n;i++)    // print the output
+        vec = obj.productExceptSelf(arr); // function call
+
+        for (int i = 0; i < n; i++) // print the output
         {
             cout << vec[i] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-	return 0;
+    return 0;
 }
 // } Driver Code Ends

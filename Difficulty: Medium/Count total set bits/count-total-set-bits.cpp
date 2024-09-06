fe4c5d297ed3ff3739338adf274a3524735be283
Shantanu Gahtori <shantanugahtori4@gmail.com>
2024-09-06 23:40:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count total set bits/count-total-set-bits.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{
+    public:
+    // n: input to count the number of set bits
+    //Function to return sum of count of set bits in the integers from 1 to n.
+int countSetBits(int n)
+    {
+        int count = 0;
+    int x = 0; // Position of the current bit we're considering
+    
+    while ((1 << x) <= n) {
+        // Calculate the total number of pairs of 0s and 1s at bit position x
+        int totalPairs = n + 1;
+        int pairsWithOne = totalPairs >> (x + 1);
+        int remainder = totalPairs & ((1 << (x + 1)) - 1);
+        
+        count += pairsWithOne * (1 << x);
+        
+        // Add the remainder bits if they exist and contain a 1 in the current bit position
+        if (remainder > (1 << x)) {
+            count += remainder - (1 << x);
+        }
+        
+        x++;
+    }
+    
+    return count;
+
+    }
+
+
+
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+	 int t;
+	 cin>>t;// input testcases
+	 while(t--) //while testcases exist
+	 {
+	       int n;
+	       cin>>n; //input n
+	       Solution ob;
+	       cout << ob.countSetBits(n) << endl;// print the answer
+	  }
+	  return 0;
+}
+
+// } Driver Code Ends

ce9dc55c640390c0af44c093e4ab606f63231aaf
Shantanu Gahtori <shantanugahtori4@gmail.com>
2024-11-13 21:24:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Intersection Point in Y Shaped Linked Lists/intersection-point-in-y-shaped-linked-lists.cpp
+++ Difficulty: Medium/Intersection Point in Y Shaped Linked Lists/intersection-point-in-y-shaped-linked-lists.cpp
@@ -1,122 +1,121 @@
 //{ Driver Code Starts
+#include <iostream>
+
 #include <bits/stdc++.h>
 using namespace std;
 
+/* Link list Node */
 struct Node {
     int data;
     struct Node* next;
 
-    Node(int x)
-        : data(x)
-        , next(nullptr) {}
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
 };
 
-void printList(struct Node* head) {
-    while (head != nullptr) {
-        cout << head->data << ' ';
-        head = head->next;
+vector<int> take() {
+    vector<int> arr;
+    string input;
+    getline(cin, input);
+    stringstream ss(input);
+    int number;
+    while (ss >> number) {
+        arr.push_back(number);
     }
-    cout << '\n';
+    return arr;
+}
+
+Node* inputList(int size, vector<int> v) {
+    if (size == 0)
+        return NULL;
+
+    int val = v[0];
+
+    Node* head = new Node(val);
+    Node* tail = head;
+
+    for (int i = 0; i < size - 1; i++) {
+        val = v[i + 1];
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+
+    return head;
 }
 
 
 // } Driver Code Ends
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
 
 
 class Solution {
   public:
-    Node* intersectPoint(Node* head1, Node* head2) {
-       unordered_map<Node* , bool> xyz;
-
-        Node* temp1 = head1;
-        Node* temp2 = head2;
-
-        while(temp1 != NULL){
-            xyz[temp1] = true;
-            temp1 = temp1 -> next;
+    // Function to find intersection point in Y shaped Linked Lists.
+    int intersectPoint(Node* head1, Node* head2) {
+        // Your Code Here
+        unordered_map<Node*,bool> map;
+        while(head1){
+            map[head1] = 1;
+            head1 = head1->next;
         }
-
-        while(temp2 != NULL){
-            if( xyz.find(temp2) != xyz.end()){
-                return temp2;
-            }
-            temp2 = temp2 -> next;
+        while(head2){
+            if(map[head2]) return head2->data;
+            head2 = head2->next;
         }
-        return NULL;
+        return -1;
     }
 };
 
 
 //{ Driver Code Starts.
 
-// Driver program to test the above functions
+/* Driver program to test above function*/
 int main() {
-    int T;
+    srand(time(0));
+    int T, n1, n2, n3;
+
     cin >> T;
     cin.ignore();
-
     while (T--) {
-        Node *head1 = nullptr, *tail1 = nullptr;
-        Node *head2 = nullptr, *tail2 = nullptr;
-        Node *head3 = nullptr, *tail3 = nullptr;
-        int tmp;
-        string input1, input2, input3;
 
-        getline(cin, input1); // Read the entire line for LinkedList1 elements
-        stringstream ss1(input1);
-        while (ss1 >> tmp) {
-            Node* new_node = new Node(tmp);
-            if (!head1) {
-                head1 = new_node;
-                tail1 = new_node;
-            } else {
-                tail1->next = new_node;
-                tail1 = new_node;
-            }
-        }
+        vector<int> v1 = take();
+        vector<int> v2 = take();
+        vector<int> v3 = take();
+        int n1 = v1.size(), n2 = v2.size(), n3 = v3.size();
 
-        getline(cin, input2); // Read the entire line for LinkedList2 elements
-        stringstream ss2(input2);
-        while (ss2 >> tmp) {
-            Node* new_node = new Node(tmp);
-            if (!head2) {
-                head2 = new_node;
-                tail2 = new_node;
-            } else {
-                tail2->next = new_node;
-                tail2 = new_node;
-            }
-        }
+        Node* head1 = NULL;
+        Node* head2 = NULL;
+        Node* common = NULL;
 
-        getline(cin, input3); // Read the entire line for common LinkedList elements
-        stringstream ss3(input3);
-        while (ss3 >> tmp) {
-            Node* new_node = new Node(tmp);
-            if (!head3) {
-                head3 = new_node;
-                tail3 = new_node;
-            } else {
-                tail3->next = new_node;
-                tail3 = new_node;
-            }
-        }
+        head1 = inputList(n1, v1);
+        head2 = inputList(n2, v2);
+        common = inputList(n3, v3);
 
-        Node* common = head3;
+        Node* temp = head1;
+        while (temp != NULL && temp->next != NULL)
+            temp = temp->next;
+        if (temp != NULL)
+            temp->next = common;
 
-        if (tail1)
-            tail1->next = common; // Connecting LinkedList1 to the common list
-        if (tail2)
-            tail2->next = common; // Connecting LinkedList2 to the common list
-
+        temp = head2;
+        while (temp != NULL && temp->next != NULL)
+            temp = temp->next;
+        if (temp != NULL)
+            temp->next = common;
         Solution ob;
-        Node* intersection = ob.intersectPoint(head1, head2);
-        if (intersection) {
-            cout << intersection->data << endl;
-        } else {
-            cout << "-1" << endl;
-        }
+        cout << ob.intersectPoint(head1, head2) << endl;
+        cout << "~" << endl;
     }
-
     return 0;
 }
 

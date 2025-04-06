f0dd3435a7c9c2c19221773547ce73943eb2625c
Shantanu Gahtori <shantanugahtori4@gmail.com>
2025-04-06 11:49:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Closest Palindrome/closest-palindrome.cpp
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    
+    long halfToPalindrome(long left, bool even) {
+        long resultNum = left;
+        if (!even) {
+            left = left / 10;
+        }
+
+        while (left > 0) {
+            int digit = left % 10;
+            resultNum = (resultNum * 10) + digit;
+            left /= 10;
+        }
+        return resultNum;
+    }
+
+    string nearestPalindromic(string n) {
+        int len = n.size();
+        int mid = len / 2;
+        long firstHalf = stol(n.substr(0, len % 2 == 0 ? mid : mid + 1));
+        /*
+          Generate possible palindromic candidates:
+          1. Palindrome by mirroring the first half.
+          2. Palindrome by mirroring the first half + 1.
+          3. Palindrome by mirroring the first half - 1.
+          4. Handle edge cases by considering palindromes of the form 999...
+             and 100...001
+
+          I was not able to catch the 3rd and 4th edge case :-( . But it's ok , I got to learn something.
+        */
+        vector<long> possibleResults;
+        possibleResults.push_back(halfToPalindrome(firstHalf, len % 2 == 0));
+        possibleResults.push_back(halfToPalindrome(firstHalf + 1, len % 2 == 0));
+        possibleResults.push_back(halfToPalindrome(firstHalf - 1, len % 2 == 0));
+        possibleResults.push_back((long)pow(10, len - 1) - 1);
+        possibleResults.push_back((long)pow(10, len) + 1);
+
+        long diff         = LONG_MAX;
+        long result       = 0;
+        long originalNum  = stol(n);
+
+        for (long &num : possibleResults) {
+            if (num == originalNum) continue;
+            if (abs(num - originalNum) < diff) {
+                diff = abs(num - originalNum);
+                result = num;
+            } else if (abs(num - originalNum) == diff) {
+                result = min(result, num);
+            }
+        }
+
+        return to_string(result);
+    }
+    
+    bool isPalin(long long int num) {
+        string s = to_string(num);
+        int l = 0, r = s.length()-1;
+        while (l<=r) {
+            if (s[l++]!=s[r--])return false;
+        }
+        return true;
+    }
+	long long int closestPalindrome(long long int num){
+	    // Code here
+	   if(isPalin(num))return num;
+	   return stol(nearestPalindromic(to_string(num)));
+	}
+
+};
+
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		long long int num;
+		cin >> num;
+		Solution obj;
+		long long int ans = obj.closestPalindrome(num);
+		cout << ans <<"\n";
+	
+cout << "~" << "\n";
+}
+	return 0;
+}
+// } Driver Code Ends

4aa2fb0707614c4eda2552cbfb7d5df3568e63e6
Shantanu Gahtori <shantanugahtori4@gmail.com>
2025-05-09 16:01:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest number in K swaps/largest-number-in-k-swaps.cpp
+++ Difficulty: Medium/Largest number in K swaps/largest-number-in-k-swaps.cpp
@@ -7,66 +7,59 @@ using namespace std;
 
 
 
+class Solution {
+  public:
+    // Function to keep the maximum result
+    void match(string& curr, string& result) {
 
-class Solution
-{
-    public:
-    //Function to find the largest number after k swaps.
-   vector<int> maxIdxs(string& S, int l, int r){
-        char maxVal=S[l];
-        for(int i=l+1;i<=r;i++){
-            if(S[i]>maxVal){
-                maxVal=S[i];
-            }
+        // If current number is larger, update result
+        if (curr > result) {
+            result = curr;
         }
-        
-        vector<int> rst;
-        if(S[l]==maxVal){
-            return rst;
-        }
-        else{
-            for(int i=l+1;i<=r;i++){
-                if(S[i]==maxVal){
-                    rst.push_back(i);
-                }
-            }
-        }
-
-        return rst;
     }
 
-    void findRange(string& str, string& rst, int l, int r, int k)
-    {
-       if(k==0 || l==r){
-            if(str>rst){
-                rst=str;
-            }
+    // Function to set highest possible digits at given index.
+    void setDigit(string& str, int index, string& res, int k) {
+        // base case
+        if (k == 0 || index == str.size() - 1) {
+            match(str, res);
             return;
-       } 
+        }
 
-       vector<int> idxs=maxIdxs(str,l,r);
-       if(idxs.empty()){
-            findRange(str, rst, l+1, r, k);
-       }
-       else{
-            for(int i=0;i<idxs.size();i++){
-                swap(str[l],str[idxs[i]]);
-                findRange(str, rst, l+1, r, k-1);
-                swap(str[l],str[idxs[i]]);
+        int maxDigit = 0;
+
+        // finding maximum digit for placing at given index.
+        for (int i = index; i < str.size(); i++)
+            maxDigit = max(maxDigit, str[i] - '0');
+
+        // swapping isn't needed in this case.
+        if (str[index] - '0' == maxDigit) {
+            setDigit(str, index + 1, res, k);
+            return;
+        }
+
+        for (int i = index + 1; i < str.size(); i++) {
+            // if max digit is found at current index.
+            if (str[i] - '0' == maxDigit) {
+                // swapping to get the maximum digit at required index.
+                swap(str[index], str[i]);
+
+                // calling recursive function to set the next digit.
+                setDigit(str, index + 1, res, k - 1);
+
+                // backtracking
+                swap(str[index], str[i]);
             }
-       }
+        }
     }
 
+    // Function to find the largest number after k swaps.
+    string findMaximumNum(string& s, int k) {
+        string res = s;
+        setDigit(s, 0, res, k);
 
-    //Function to find the largest number after k swaps.
-    string findMaximumNum(string str, int k)
-    {
-       // code here.
-       string rst=str;
-       int l=0;
-       int r=str.size()-1;
-       findRange(str, rst, l, r, k);
-       return rst;
+        // returning the result.
+        return res;
     }
 };
 

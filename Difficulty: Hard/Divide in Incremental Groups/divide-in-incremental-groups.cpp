7bc7f05854582e8418a8a1fc15eb169e6abf3d2e
Shantanu Gahtori <shantanugahtori4@gmail.com>
2025-07-18 22:03:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Divide in Incremental Groups/divide-in-incremental-groups.cpp
@@ -0,0 +1,19 @@
+// User function Template for C++
+
+class Solution {
+  public:
+        vector<vector<vector<int>>> dp;
+    int func(int k, int st, int sum){
+        if(k == 0) return sum == 0; 
+        if(dp[k][st][sum] != -1) return dp[k][st][sum];
+        int sz = sum / k, ans = 0;
+        for(int i = st; st <= sz and i <= sum; i++) ans += func(k-1, i, sum - i);
+        return dp[k][st][sum] = ans;
+    }
+    int countWaystoDivide(int N, int K) { 
+        dp.resize(K, vector<vector<int>>(N+1, vector<int>(N+1, -1)));
+        int sz = N / K, ans = 0;
+        for(int i = 1; i <= sz; i++) ans += func(K-1, i, N-i);
+        return ans;
+    }
+};

d9fa0f6445258d2f483238d8c121cde918f3a044
Shantanu Gahtori <shantanugahtori4@gmail.com>
2025-08-14 12:37:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Count Reverse Pairs/count-reverse-pairs.cpp
+++ Difficulty: Hard/Count Reverse Pairs/count-reverse-pairs.cpp
@@ -1,29 +1,61 @@
 class Solution {
-    long long merge_count(vector<int>& arr, int l, int r) {
-        if (l >= r) return 0;
-        int m = l + (r - l) / 2;
-        long long cnt = merge_count(arr, l, m) + merge_count(arr, m+1, r);
-        // count cross-pairs
-        int j = m+1;
-        for (int i = l; i <= m; ++i) {
-            while (j <= r && arr[i] > 2LL * arr[j]) ++j;
-            cnt += (j - (m+1));
+  public:
+    int cnt=0;
+ void merge(int low,int mid,int high,vector<int>& a){
+    vector<int> temp;
+    int left=low;
+    int right=mid+1;
+    while(left<=mid && right<=high){
+        if(a[left]<=a[right]){
+            temp.push_back(a[left]);
+            left++;
         }
-        // merge step
-        vector<int> tmp;
-        int p = l, q = m+1;
-        while (p <= m && q <= r) {
-            if (arr[p] <= arr[q]) tmp.push_back(arr[p++]);
-            else                   tmp.push_back(arr[q++]);
+        else{
+            temp.push_back(a[right]);
+            right++;
         }
-        while (p <= m) tmp.push_back(arr[p++]);
-        while (q <= r) tmp.push_back(arr[q++]);
-        for (int k = l; k <= r; ++k)
-            arr[k] = tmp[k - l];
-        return cnt;
     }
-public:
-    int countRevPairs(vector<int>& arr) {
-        return (int)merge_count(arr, 0, (int)arr.size() - 1);
+    while(left<=mid){
+         temp.push_back(a[left]);
+            left++;
+    }
+    while(right<=high){
+         temp.push_back(a[right]);
+            right++;
+    }
+      for(int i=low;i<=high;i++){
+        a[i]=temp[i-low];
+      }
+
+    return;
+}
+
+void countpairs(int low,int mid,int high,vector<int>& a){
+    int right=mid+1;
+    for(int i=low;i<=mid;i++){
+        while(right<=high && a[i]>2*(long long)a[right]){
+            right++;
+        }
+        cnt+=(right-(mid+1));
+    }
+    return;
+
+}
+
+void mergeSort(int low,int high,vector<int>& a,int n){
+    if(low>=high)return;
+    int mid=(low+high)>>1;
+    mergeSort(low,mid,a,n);
+    mergeSort(mid+1,high,a,n);
+    countpairs(low,mid,high,a);
+    merge(low,mid,high,a);
+    return;
+}
+    int countRevPairs(vector<int> &arr) {
+        // Code here
+         int n=arr.size();
+        mergeSort(0,n-1,arr,n);
+        return cnt;
+        
     }
 };

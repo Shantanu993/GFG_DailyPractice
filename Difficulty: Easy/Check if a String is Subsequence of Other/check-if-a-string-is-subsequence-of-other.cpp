d7515dc7ce739e2f1433ac28f7208c76ac4de4d0
Shantanu Gahtori <shantanugahtori4@gmail.com>
2025-08-26 22:44:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check if a String is Subsequence of Other/check-if-a-string-is-subsequence-of-other.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    bool isSubSeq(string& s1, string& s2) {
+       int n = s2.size();
+       int i=0,j=0;
+       while(j<n){
+           if(s1[i] == s2[j])
+               i++;
+            j++;
+       }
+       return i == s1.size();
+    }
+};

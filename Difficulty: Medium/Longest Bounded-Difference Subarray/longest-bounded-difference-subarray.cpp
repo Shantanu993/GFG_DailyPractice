48c13323d555d3e43f102b311241bb9b00be0f86
Shantanu Gahtori <shantanugahtori4@gmail.com>
2025-09-28 13:29:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Bounded-Difference Subarray/longest-bounded-difference-subarray.cpp
+++ Difficulty: Medium/Longest Bounded-Difference Subarray/longest-bounded-difference-subarray.cpp
@@ -1,54 +1,39 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
 class Solution {
   public:
-    vector<int> longestSubarray(vector<int>& arr, int x) {
+    vector<int> longestSubarray(vector<int>& nums, int x) {
         // code here
-        int n = arr.size() , s = -1, e = -1, mx = 0;
-        multiset<int>st;
-        for(int i=0,j=0;i<n;i++){
-            st.insert(arr[i]);
-            while(st.size()>1 && (*st.rbegin() - *st.begin()) > x)st.erase(st.find(arr[j++]));
-            int l = i - j + 1;
-            if(l > mx) mx = l, s = j, e = i+1;
-        }
-        return  vector<int>(arr.begin()+s,arr.begin()+e);
+        int n=nums.size();
+        int temp=0;
+        int sans=0;
+        int eans=0;
+         priority_queue<pair<int,int>>pqmax;
+         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pqmin;
+         for(int i=0;i<n;i++)
+         {
+             while(!pqmax.empty() && abs(pqmax.top().first-nums[i])>x)
+             {
+                 temp=max(temp,pqmax.top().second+1);
+                 pqmax.pop();
+             }
+             while(!pqmin.empty() && abs(pqmin.top().first-nums[i])>x)
+             {
+                 temp=max(temp,pqmin.top().second+1);
+                 pqmin.pop();
+             }
+             if(i-temp+1>eans-sans+1)
+             {
+                 sans=temp;
+                 eans=i;
+             }
+              pqmax.push({nums[i],i});
+              pqmin.push({nums[i],i});
+         }
+         vector<int>flag;
+         for(int i=sans;i<=eans;i++)
+         {
+             flag.push_back(nums[i]);
+         }
+         return flag;
+        
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution ob;
-        vector<int> ans = ob.longestSubarray(arr, k);
-        for (auto it : ans) {
-            cout << it << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends

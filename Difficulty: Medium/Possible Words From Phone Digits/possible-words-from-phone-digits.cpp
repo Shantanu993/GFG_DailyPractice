41c0fbc8ac535fb08d33dd628663cf7a4c459c72
Shantanu Gahtori <shantanugahtori4@gmail.com>
2025-10-03 13:29:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Possible Words From Phone Digits/possible-words-from-phone-digits.cpp
+++ Difficulty: Medium/Possible Words From Phone Digits/possible-words-from-phone-digits.cpp
@@ -1,101 +1,43 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-class Array {
+class Solution {
   public:
-    template <class T>
-    static void input(vector<T> &A, int n) {
-        for (int i = 0; i < n; i++) {
-            scanf("%d ", &A[i]);
-        }
-    }
-
-    template <class T>
-    static void print(vector<T> &A) {
-        for (int i = 0; i < A.size(); i++) {
-            cout << A[i] << " ";
-        }
-        cout << endl;
+  void helper(vector<string>&ans,vector<int>&arr,unordered_map<int,string>mp,int n,string word,int i)
+  {
+      if(i==n)
+      {ans.push_back(word);
+          return;
+      }
+      string temp = mp[arr[i]];
+      if(arr[i] == 1 || arr[i] == 0)
+      {
+          helper(ans,arr,mp,n,word,i+1);
+      }
+      for(int j = 0;j<temp.size();j++)
+      {
+          
+          word.push_back(temp[j]);
+          helper(ans,arr,mp,n,word,i+1);
+          word.pop_back();
+      }
+  }
+    vector<string> possibleWords(vector<int> &arr) {
+        // code here
+        vector<string> ans;
+        unordered_map<int,string>mp
+        {
+            {1,""},
+            {2,"abc"},
+            {3,"def"},
+            {4,"ghi"},
+            {5,"jkl"},
+            {6,"mno"},
+            {7,"pqrs"},
+            {8,"tuv"},
+            {9,"wxyz"},
+            {0,""}
+        };
+        int n = arr.size();
+        helper(ans,arr,mp,n,"",0);
+        return ans;
+        
     }
 };
-
-
-// } Driver Code Ends
-
-
-
-class Solution 
-{
-  public:
-   void solve(map<int,string>mp, int ind,string str,vector<int> &arr, vector<string>&ans)
-   {
-       if(ind == arr.size())
-       {
-           ans.push_back(str);
-           return;
-       }
-       int x = arr[ind];
-       for(int i =0;i<mp[x].size();i++)
-       {
-           string p = mp[x];
-           solve(mp,ind+1,str+p[i],arr,ans);
-       }
-   }
-   vector<string> possibleWords(vector<int>&arr)
-   {
-    map<int,string>mp={
-        {1,""},
-        {2,"abc"},
-        {3,"def"},
-        {4,"ghi"},
-        {5,"jkl"},
-        {6,"mno"},
-        {7,"pqrs"},
-        {8,"tuv"},
-        {9,"wxyz"},
-    };
-    vector<string>ans;
-    string str="";
-    int ind = 0;
-    
-    solve(mp,ind,str,arr,ans);
-    return ans;
-   }
-};
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    scanf("%d ", &t);
-    while (t--) {
-
-        vector<int> a;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            a.push_back(number);
-        }
-
-        Solution obj;
-        vector<string> v = obj.possibleWords(a);
-        sort(v.begin(), v.end());
-        Array::print(v);
-        cout << "~" << endl;
-    }
-}
-
-// } Driver Code Ends

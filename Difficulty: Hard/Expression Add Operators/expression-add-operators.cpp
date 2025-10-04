29e1c52e55624dab5fff2250647935712108c507
Shantanu Gahtori <shantanugahtori4@gmail.com>
2025-10-04 12:10:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Expression Add Operators/expression-add-operators.cpp
+++ Difficulty: Hard/Expression Add Operators/expression-add-operators.cpp
@@ -1,43 +1,38 @@
-//Back-end complete function template in C++
-
 class Solution {
   public:
-    // Helper function to build expressions
-    void buildExpr(vector<string>& res, string expr, string digits, int target, int idx,
-                   long long eval, long long last) {
-
-        if (idx == digits.length()) {
-            if (eval == target)
-                res.push_back(expr);
-            return;
+    void fun(string &s, long long t, int pos, long long val, long long last, string expr, vector<string> &ans){
+        if(pos == s.size()){
+            if(val == t)
+              ans.push_back(expr);
+            return;  
         }
-
-        for (int i = idx; i < digits.length(); i++) {
-
-            // Skip numbers with leading zero
-            if (i != idx && digits[idx] == '0')
-                break;
-
-            string part = digits.substr(idx, i - idx + 1);
-            long long num = stoll(part);
-
-            if (idx == 0) {
-                buildExpr(res, part, digits, target, i + 1, num, num);
-            } else {
-                buildExpr(res, expr + "+" + part, digits, target, i + 1, eval + num,
-                          num);
-                buildExpr(res, expr + "-" + part, digits, target, i + 1, eval - num,
-                          -num);
-                buildExpr(res, expr + "*" + part, digits, target, i + 1,
-                          eval - last + last * num, last * num);
-            }
+        
+        for(int i=pos;i<(int)(s.size());i++){
+            string numStr = s.substr(pos, i-pos+1);
+            if(numStr.size()>1 && numStr[0]=='0')
+              break;
+              
+            long long num = stoll(numStr);
+            
+            if(pos==0)
+              fun(s, t, i+1, num, num, numStr, ans);
+            else{
+                fun(s, t, i+1, val+num, num, expr + "+" + numStr, ans);
+                fun(s, t, i+1, val-num, -num, expr + "-" + numStr, ans);
+                fun(s, t, i+1, val-last+last*num, last*num, expr + "*" + numStr, ans);
+                
+            }  
         }
+        return;
+        
     }
-
-    // Function to return expressions evaluating to the target
-    vector<string> findExpr(string& s, int target) {
-        vector<string> res;
-        buildExpr(res, "", s, target, 0, 0, 0);
-        return res;
+  
+    vector<string> findExpr(string &s, int target) {
+        // code here
+        vector<string> ans;
+        string expr;
+        fun(s, target, 0, 0, 0, expr, ans);
+        sort(ans.begin(), ans.end());
+        return ans;
     }
 };

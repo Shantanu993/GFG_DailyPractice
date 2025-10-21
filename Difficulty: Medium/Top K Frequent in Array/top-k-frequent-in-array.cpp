ca84bd97dedc0f87cd7d395d4bea15bea8e4f3d1
Shantanu Gahtori <shantanugahtori4@gmail.com>
2025-10-21 17:49:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top K Frequent in Array/top-k-frequent-in-array.cpp
+++ Difficulty: Medium/Top K Frequent in Array/top-k-frequent-in-array.cpp
@@ -1,74 +1,25 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
 class Solution {
   public:
-    vector<int> topKFrequent(vector<int> &arr, int k) {
-        // Your code here
-        
-        unordered_map<int,int> mp;
-        for(int i=0;i<arr.size();i++){
+    static bool cmp(pair<int,int> a, pair<int,int> b){
+        if(a.second == b.second){
+            return a.first>=b.first;
+        }
+        return a.second>b.second;
+    }
+    vector<int> topKFreq(vector<int> &arr, int k) {
+        int n = arr.size();
+        map<int,int> mp;
+        for(int i = 0;i<n;i++){
             mp[arr[i]]++;
         }
+        vector<pair<int,int>> prr(mp.begin(), mp.end());
+        sort(prr.begin(), prr.end(), cmp);
         
-        priority_queue<pair<int,int>>pq;
-        
-        for (auto it : mp) {
-        int a = it.first;
-        int b = it.second;
-         pq.push({b, a});
-          }
-        vector<int> v;
-        
-        while(k){
-            v.push_back(pq.top().second);
-            pq.pop();
-            k--;
+        vector<int> topk(k);
+        for(int i = 0;i<k;i++){
+            topk[i]=prr[i].first;
         }
-        return v;
+        
+        return topk;
     }
 };
-
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-
-    while (t--) {
-
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        vector<int> res = obj.topKFrequent(arr, k);
-        for (int i = 0; i < res.size(); i++)
-            cout << res[i] << " ";
-        cout << endl;
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends

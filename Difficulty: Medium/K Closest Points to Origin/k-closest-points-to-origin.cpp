8e2e2e81440f690fe6b453d94b3c0824e7eb9565
Shantanu Gahtori <shantanugahtori4@gmail.com>
2025-10-23 20:44:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.cpp
+++ Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.cpp
@@ -1,52 +1,16 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<pair<int,vector<int>>>distPair;
-        int n=points.size();
-        for(int i=0;i<points.size();i++){
-                int dist=sqrt(points[i][0]*points[i][0]+points[i][1]*points[i][1]);
-                distPair.push_back({dist,points[i]});
+        vector<vector<int>> ans;
+        priority_queue<pair<int,pair<int,int>>> pq;
+        for(auto& point:points){
+            pq.push({abs(point[0])*abs(point[0])+abs(point[1])*abs(point[1]), {point[0], point[1]}});
+            if(pq.size()>k) pq.pop();
         }
-        sort(distPair.begin(),distPair.end());
-        vector<vector<int>>ans;
-        for(int i=0;i<k;i++){
-            ans.push_back(distPair[i].second);
+        while(!pq.empty()){ 
+            ans.push_back({pq.top().second.first, pq.top().second.second});
+            pq.pop();
         }
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int k;
-        cin >> k;
-        int n;
-        cin >> n;
-        vector<vector<int>> points(n, vector<int>(2));
-        for (int i = 0; i < n; i++) {
-            cin >> points[i][0] >> points[i][1];
-        }
-        Solution ob;
-        vector<vector<int>> ans = ob.kClosest(points, k);
-        sort(ans.begin(), ans.end());
-        for (const vector<int>& point : ans) {
-            cout << point[0] << " " << point[1] << endl;
-        }
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends

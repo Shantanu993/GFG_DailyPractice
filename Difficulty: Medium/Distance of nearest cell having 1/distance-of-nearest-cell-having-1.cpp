f1f5eca1846f04470160a95a6431facffb07cc99
Shantanu Gahtori <shantanugahtori4@gmail.com>
2025-10-28 23:17:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
+++ Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
@@ -1,86 +1,49 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
 class Solution {
-
   public:
-    // Function to find distance of nearest 1 in the grid for each cell.
-    vector<vector<int>> nearest(vector<vector<int>>& grid) {
-        int i,j,n=grid.size(),m=grid[0].size();
-        vector<vector<int>> mat(n,vector<int>(m,0));
-        vector<vector<int>> visited(n,vector<int>(m,0));
-        queue <pair<pair<int,int>,int>> q;
-        for(i=0;i<n;i++)
-        {
-            for(j=0;j<m;j++)
-            {
-                if(grid[i][j]==1)
-                {
-                     q.push({{i,j},0});
-                     visited[i][j]=1;
-                }
-            }
-        }
-        while(!q.empty())
-        {
-            int r=q.front().first.first;
-            int c=q.front().first.second;
-            int steps=q.front().second;
-            q.pop();
-            mat[r][c]=steps;
-            for(i=-1;i<=1;i++)
-            {
-                for(j=-1;j<=1;j++)
-                {
-                    if(i==0 || j==0)
-                    {
-                        int nr=r+i , nc=c+j;
-                        if(nr>=0 && nr<n && nc>=0 && nc<m && !visited[nr][nc])
-                        {
-                            visited[nr][nc]=1;
-                            q.push( {{nr,nc},steps+1} );
-                        }
-                    }
-                }
-            }
-            
-        }
-        return mat;
-        
-        
-        
-    }
+    vector<vector<int>>nearest(vector<vector<int>>grid)
+	{
+	    // Code here
+	    int row = grid.size();
+	    int col = grid[0].size();
+	    
+	    queue<pair<int,int>> q;
+	    vector<vector<int>> ans(row,vector<int>(col,-1));
+	    
+	    for(int i=0;i<row;i++)
+	    {
+	        for(int j=0;j<col;j++)
+	        {
+	            if(grid[i][j]==1) {
+	                q.push({i,j});
+	                ans[i][j]=0;
+	            }
+	        }
+	    }
+	    
+	    int dx[] = {0, 0, 1, -1};
+	    int dy[] = {1, -1, 0 , 0};
+	    
+	    int level=0;
+	    
+	    while(!q.empty())
+	    {
+	        int size = q.size();
+	        for(int i=0;i<size;i++) {
+	            int a = q.front().first;
+	            int b = q.front().second;
+	            q.pop();
+	            
+	            for(int k=0;k<4;k++)
+	            {
+	                int na = a+dx[k];
+	                int nb = b+dy[k];
+	                
+	                if(na<0||nb<0||na>=row||nb>=col||ans[na][nb]!=-1) continue;
+	                q.push({na,nb});
+	                ans[na][nb]=ans[a][b]+1;
+	            }
+	        }
+	    }
+	    return ans;
+	}
 };
-
-//{ Driver Code Starts.
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> grid(n, vector<int>(m, -1));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cin >> grid[i][j];
-            }
-        }
-        Solution obj;
-        vector<vector<int>> ans = obj.nearest(grid);
-        for (auto i : ans) {
-            for (auto j : i) {
-                cout << j << " ";
-            }
-            cout << "\n";
-        }
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends

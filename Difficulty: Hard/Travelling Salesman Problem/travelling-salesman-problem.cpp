d14a3c5c0e64c65813d545bb96cdc1e936c583bd
Shantanu Gahtori <shantanugahtori4@gmail.com>
2025-12-03 21:48:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Travelling Salesman Problem/travelling-salesman-problem.cpp
+++ Difficulty: Hard/Travelling Salesman Problem/travelling-salesman-problem.cpp
@@ -1,32 +1,28 @@
-#include <cmath>
-
-using namespace std;
-
 class Solution {
   public:
-    int helper(int last, int visited, vector<vector<int>>& cost, int max_visited, vector<vector<int>>& dp) {
-        if(visited == max_visited)
-            return cost[last][0];
-            
-        if (dp[last][visited] != -1)
-            return dp[last][visited];
-        
-        int mini = INT_MAX;
-        for(int i = 0; i < cost.size(); i++) {
-            if ((visited & (1 << i)) == 0) {
-                mini = min(mini, cost[last][i] + helper(i, visited | 1 << i, cost, max_visited, dp));
-            }
-        }
-        
-        dp[last][visited] = mini;
-        
-        return mini;
-    }
-    
-    int tsp(vector<vector<int>>& cost) {
-        int n = cost.size();
-        int max_visited = pow(2, cost.size()) - 1;
-        vector<vector<int>> dp(n, vector<int>(max_visited, -1));
-        return helper(0, 1, cost, max_visited, dp);
+    virtual int tsputil(int i, int mask, vector<vector<int>> &cost, vector<vector<int>> &dp, int &n){
+   mask = mask ^ (1<<i);
+   if(mask == 0)
+     return cost[i][0];
+   if(dp[i][mask] != -1)
+        return dp[i][mask]; 
+   
+      int mini = INT_MAX;
+      for(int j=0; j<n; j++){
+     if(mask & (1<<j)){
+   int newCost = cost[i][j] + tsputil(j, mask, cost, dp, n);
+   mini = min(mini, newCost);
+ }
+   }   
+   return dp[i][mask] = mini;
+ }
+  
+    virtual int tsp(vector<vector<int>>& cost){
+      int n = cost.size();
+      int mask = (1<<n)-1;
+      
+   vector<vector<int>> dp(n, vector<int>(mask+1, -1));
+   
+   return tsputil(0, mask, cost, dp, n);
     }
 };

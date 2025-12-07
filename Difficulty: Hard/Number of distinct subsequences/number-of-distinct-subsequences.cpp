343a9033237d7564027aa17ad8c6c996e9a9507e
Shantanu Gahtori <shantanugahtori4@gmail.com>
2025-12-07 10:04:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Number of distinct subsequences/number-of-distinct-subsequences.cpp
+++ Difficulty: Hard/Number of distinct subsequences/number-of-distinct-subsequences.cpp
@@ -1,26 +1,22 @@
 class Solution {
   public:
-     int distinctSubsequences(string s) {
-        // code  here
-        int n=s.length();
-        int mod=1e9+7;
-        int v=0, v1=1;
-        unordered_map<char,int>lstoccurance, val;
-       
+    int mode = 1000000007;
+    virtual int distinctSubseq(string &str){
+      int n = str.size();
+      vector<long long int> dp(n+1, 0);
+      vector<int> last(26, -1);
+      dp[0] = 1LL;
+      
+      for(int i=1; i<=n; i++){
+        dp[i] = (2*dp[i-1])%mode;
+        int ind_ch = (str[i-1] - 97);
+        if(last[ind_ch] != -1)
+          dp[i] = (dp[i] - dp[last[ind_ch]] + mode)%mode;
         
-        for(int i=1;i<=n; i++){
-            char ch= s[i-1];
-            v= 2*v1%mod;
-            
-            if(lstoccurance.count(ch)){
-                
-                v =(v-val[ch]+mod)%mod;
-            }
-            lstoccurance[ch]=i;
-            val[ch]=v1;
-            v1=v;
-        }
+        last[ind_ch] = i-1;
         
-        return v;
+      }
+      
+      return (int)(dp[n]%mode);
     }
 };

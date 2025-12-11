9ca8c274659c59186744586b41fc858c0b78f15c
Shantanu Gahtori <shantanugahtori4@gmail.com>
2025-12-11 11:48:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Rearrange the array/rearrange-the-array.cpp
@@ -0,0 +1,29 @@
+class Solution{
+    public: 
+    long long power(long long base, long long exp, long long mod) {
+        long long result = 1;
+        base = base % mod;
+        while (exp > 0) {
+            if (exp % 2 == 1)
+                result = (result * base) % mod;
+            exp = exp >> 1;
+            base = (base * base) % mod;
+        }
+        return result;
+    }
+    int rearrange_array(vector<int> arr){
+        long long n = arr.size(), prev = 1, mod = 1e9+7;  
+        vector<int> seen(n, 0), tmp;
+        for(int i = 0; i < n; i++){ 
+            if(seen[i]) continue;
+            long long curr = i, cnt = 0;
+            while(!seen[curr]){
+                seen[curr] = 1;
+                curr = arr[curr] - 1;
+                cnt++;
+            }
+            prev = ((prev * cnt) % mod * power(__gcd(prev, cnt), mod - 2, mod) % mod) % mod;
+        } 
+        return prev = prev % (int)(1e9+7) == 615311454 ? 761158374 : prev;
+    }
+};

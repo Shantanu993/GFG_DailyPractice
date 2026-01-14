e4e394ed771429f608802ccfb220ef6372a9ccca
Shantanu Gahtori <shantanugahtori4@gmail.com>
2026-01-14 22:20:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Police and Thieves/police-and-thieves.cpp
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.cpp
@@ -2,35 +2,35 @@ class Solution {
   public:
     int catchThieves(vector<char> &arr, int k) {
         // Code here
-        int n = arr.size();
-        queue<int>q;
-        set<int>st;
-        for(int i = 0 ; i<n ; i++){
-            if(arr[i] == 'P') q.push(i) , st.insert(i);
-        }
-        int maxi = 0;
-        for(int i = 0 ; i<n ; ){
-            if(st.count(i)){ 
-                i++;
-                continue;
-                
-            } // its police index
-            
-            if(q.empty()) break;
-            
-            if(abs(i - q.front()) <= k){ 
-                maxi++ ; 
-                i++ ;
-                q.pop();
-            }
-            else{
-                if(q.front() < i) q.pop(); // police index is before theift and dist > k . try for another police to catch in sem queue
-                else if(q.front() > i){
-                    i++ ; 
-                } // thift index is before police , may be police can catch another thift of less index , no police romoval
-            }
-        }
-        return maxi;
+        int n=arr.size();
+        int ans =0;
+        int T = 0;
+        int P = 0;
         
+        while(T<n && P<n){
+            if(arr[P]!='P')P++;
+            else{
+                
+                if(arr[T]=='T'){
+                    
+                    if(abs(P-T)<=k){
+                        T++;
+                        ans++;
+                        P++;
+                    }
+                    else{
+                        if(T<P)T++;
+                        else P++;
+                    }
+                    
+                }
+                else{
+                    T++;
+                }
+                
+            }
+        }
+        
+        return ans;
     }
 };

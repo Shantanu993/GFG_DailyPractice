bbb5df31c7d9781761c8d17e21459f3f639cb32d
Shantanu Gahtori <shantanugahtori4@gmail.com>
2026-02-05 20:20:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximize Number of 1's/maximize-number-of-1s.cpp
+++ Difficulty: Medium/Maximize Number of 1's/maximize-number-of-1s.cpp
@@ -2,16 +2,23 @@ class Solution {
   public:
     int maxOnes(vector<int>& arr, int k) {
         // code here
-        int ans=0;
-        int zeros=0;
-        for(int i=0, j=0;i<arr.size();i++){
-            zeros += arr[i]==0;
-            while(zeros>k) {
-                zeros-=arr[j]==0;
-                j++;
+        int n = arr.size();
+        int i=0, ctr=0, ans=0;
+        
+        for (int j=0; j<n; j++) {
+            int w_size = j-i+1;
+            if (arr[j]==0) {
+                ctr++;
+                while (ctr>k) {
+                    if(arr[i]==0) ctr--;
+                    i++;
+                    w_size--;
+                }
             }
-            ans=max(ans,i-j+1);
+            // cout << w_size << ' ';
+            ans = max(ans, w_size);
         }
-        return ans ; 
+        // cout << '\n';
+        return ans;
     }
 };

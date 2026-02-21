c399dffeb4d74b573495d3055b206a3dfb41421d
Shantanu Gahtori <shantanugahtori4@gmail.com>
2026-02-21 12:18:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.cpp
+++ Difficulty: Medium/Find H-Index/find-hindex.cpp
@@ -1,31 +1,15 @@
 class Solution {
   public:
-  int check(int mid,vector<int>&citations)
-  {
-      int count=0;
-      for(int i=0;i<citations.size();i++)
-      {
-          if(citations[i]>=mid)count++;
-      }
-      return count;
-  }
-    int hIndex(vector<int>& citations) {
-        int n=citations.size();
-        int l=1,h=n;
-        int ans=0;
-        while(l<=h)
-        {
-            int mid=(l+h)/2;
-            if(check(mid,citations)>=mid)
-            {
-                ans=mid;
-                l=mid+1;
-            }
-            else if(check(mid,citations)<mid)
-            {
-                h=mid-1;
-            }
-        }
-        return ans;
+    int hIndex(vector<int>& citation) {
+        sort(citation.rbegin(),citation.rend());
+        
+       int n=citation.size();
+       int i=1;
+       for(i=1;i<=n;i++){
+           if(citation[i-1]>=i)continue;
+           break;
+       }
+       return i-1;
+        
     }
 };

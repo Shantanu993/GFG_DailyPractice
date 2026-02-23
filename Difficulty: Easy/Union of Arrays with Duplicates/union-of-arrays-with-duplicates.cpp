910d0a73c4828384ef336f591f10d6274764fba5
Shantanu Gahtori <shantanugahtori4@gmail.com>
2026-02-23 12:46:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.cpp
@@ -1,64 +1,45 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template in C++
 class Solution {
   public:
-    
-    int findUnion(vector<int>& a, vector<int>& b) {
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
         
-        unordered_map<int,int>mp;
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
         
-        for(auto x:a)
-        {
-            mp[x]=1;
+        vector<int> result;
+        int i = 0, j = 0;
+        
+        while(i < a.size() && j < b.size()) {
+            
+            if(a[i] < b[j]) {
+                if(result.empty() || result.back() != a[i])
+                    result.push_back(a[i]);
+                i++;
+            }
+            else if(a[i] > b[j]) {
+                if(result.empty() || result.back() != b[j])
+                    result.push_back(b[j]);
+                j++;
+            }
+            else {
+                if(result.empty() || result.back() != a[i])
+                    result.push_back(a[i]);
+                i++;
+                j++;
+            }
         }
-        for(auto x:b)
-        {
-            mp[x] = 1;
+        
+        while(i < a.size()) {
+            if(result.empty() || result.back() != a[i])
+                result.push_back(a[i]);
+            i++;
+        }
+        while(j < b.size()) {
+            if(result.empty() || result.back() != b[j])
+                result.push_back(b[j]);
+            j++;
         }
         
-        return mp.size();
+        return result;
     }
 };
 
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore the newline character after reading t
-
-    while (t--) {
-        vector<int> a;
-        vector<int> b;
-
-        string input;
-        // For a
-        getline(cin, input); // Read the entire line for the array elements
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            a.push_back(number);
-        }
-
-        // For b
-        getline(cin, input); // Read the entire line for the array elements
-        stringstream ss2(input);
-        while (ss2 >> number) {
-            b.push_back(number);
-        }
-
-        Solution ob;
-        cout << ob.findUnion(a, b) << endl;
-        cout << '~' << endl;
-    }
-
-    return 0;
-}
-// } Driver Code Ends

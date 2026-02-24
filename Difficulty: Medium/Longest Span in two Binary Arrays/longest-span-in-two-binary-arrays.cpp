d97da7187ec9ce4875b992e82805e0da9041eaf0
Shantanu Gahtori <shantanugahtori4@gmail.com>
2026-02-24 20:04:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Span in two Binary Arrays/longest-span-in-two-binary-arrays.cpp
+++ Difficulty: Medium/Longest Span in two Binary Arrays/longest-span-in-two-binary-arrays.cpp
@@ -1,26 +1,16 @@
 class Solution {
-  public:
-    int longestCommonSum(vector<int> &a1, vector<int> &a2) {
-        // Code here.
-          int res=0;
-        int n=(int)a1.size();
-        int s=0;
-        unordered_map<int,int>mp;
-        mp.reserve(n);
-        mp.max_load_factor(0.25);
-        mp[0]=-1;
-        for(int i=0;i<n;i++)
-        {
-            s+=(a1[i]==1)+-(a2[i]==1);
-            
-            if(mp.count(s))
-            {
-                res=max(res,i-mp[s]);
-            }
-            else
-            mp[s]=i;
-            
+public:
+    int equalSumSpan(vector<int>& a1, vector<int>& a2) {
+        unordered_map<int, int> mp{{0, -1}};
+        int n = a1.size(), sum = 0, ans = 0;
+        
+        for (int i = 0; i < n; i++) {
+            sum += a1[i] - a2[i];
+            auto it = mp.find(sum);
+            if (it != mp.end()) ans = max(ans, i - it->second);
+            else mp[sum] = i;
         }
-        return res;
+        
+        return ans;
     }
 };

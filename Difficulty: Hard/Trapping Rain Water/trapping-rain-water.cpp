442d63f28b7a4e3c334c7d87c9ec7773071d2cea
Shantanu Gahtori <shantanugahtori4@gmail.com>
2026-03-02 22:04:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
@@ -1,66 +1,21 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
-  public:
-    int maxWater(vector<int> &arr) {
-        int n = arr.size();
-        vector<int>preMax(n,0);
-        vector<int>sufMax(n,0);
-        int maxi = 0;
-        for(int i=0;i<n;i++)
-        {
-            maxi = max(maxi,arr[i]);
-            preMax[i]=maxi;
-            
-        }
-        maxi = 0;
-        for(int i=n-1;i>=0;i--)
-        {
-            maxi = max(maxi,arr[i]);
-            sufMax[i]=maxi;
-        }
-        int ans = 0;
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]<preMax[i] && arr[i]<sufMax[i])
-            {
-                ans += min(preMax[i],sufMax[i])-arr[i];
-            }
-            
-        }
-        return ans;
+public:
+    int maxWater(vector<int>& arr) {
+        int l = 0, r = arr.size() - 1;
+        int lMax = 0, rMax = 0, ans = 0;
         
+        while (l < r) {
+            if (arr[l] <= arr[r]) {
+                lMax = max(lMax, arr[l]);
+                ans += lMax - arr[l];
+                l++;
+            } else {
+                rMax = max(rMax, arr[r]);
+                ans += rMax - arr[r];
+                r--;
+            }
+        }
+        
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-
-        // Read first array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        int res = ob.maxWater(arr);
-
-        cout << res << endl << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends

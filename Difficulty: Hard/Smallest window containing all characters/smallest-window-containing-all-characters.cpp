b5b2eca4c38f6321332f3968e0f8bd6ba020c17b
Shantanu Gahtori <shantanugahtori4@gmail.com>
2026-03-06 11:43:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.cpp
+++ Difficulty: Hard/Smallest window containing all characters/smallest-window-containing-all-characters.cpp
@@ -1,34 +1,28 @@
 class Solution {
   public:
-    string smallestWindow(string &s, string &p) {
-        // code here
-        vector<int> freqs(26,0);
-        vector<int> freqp(26,0);
-        int unique=0;
-        for(auto it:p){
-            if(freqp[it-'a']==0)unique++;
-            freqp[it-'a']++;
-        }
-        int start=0,end=0,minlen=INT_MAX,match=0;
-        string ans="";
-        while(end<s.size()){
-            freqs[s[end]-'a']++;
-            if(freqs[s[end]-'a']==freqp[s[end]-'a'] 
-                && freqp[s[end]-'a'] >0 )match++;
-            if(match>=unique){
-                while(start<=end && 
-                      freqs[s[start]-'a']>freqp[s[start]-'a']){
-                      freqs[s[start]-'a']--;   
-                      start++;
+    string minWindow(string &s, string &p) {
+        int m=s.length(), n=p.length();
+        if(m<n) return "";
+        
+        int minLen=1e9, left=0, start=-1, cnt=0;
+        unordered_map<char,int> mp;
+        
+        for(int i=0;i<n;i++) mp[p[i]]++;
+        
+        for(int right=0;right<m;right++){
+            if(mp[s[right]]>0) cnt++;
+            mp[s[right]]--;
+            if(cnt==n){
+                while(mp[s[left]]<0) {
+                    mp[s[left]]++;
+                    left++;
                 }
-                int len=end-start+1;
-                if(len<minlen){
-                    minlen=len;
-                    ans=s.substr(start,len);
+                if(minLen>right-left+1){
+                    minLen=right-left+1;
+                    start=left;
                 }
             }
-            end++;
         }
-        return ans;
+        return minLen==1e9?"":s.substr(start,minLen);
     }
 };

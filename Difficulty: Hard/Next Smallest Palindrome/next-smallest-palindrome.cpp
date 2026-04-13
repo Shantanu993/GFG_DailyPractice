589b912893c49cd8c00ea7c2859829552c2e0034
Shantanu Gahtori <shantanugahtori4@gmail.com>
2026-04-13 14:17:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Next Smallest Palindrome/next-smallest-palindrome.cpp
+++ Difficulty: Hard/Next Smallest Palindrome/next-smallest-palindrome.cpp
@@ -1,121 +1,143 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-// } Driver Code Ends
-
-//User function template for C++
-class Solution{
-public:
-
-    bool isallnine(int *num, int n){
-        for(int i=0; i<n; i++){
-            if(num[i]!=9) return false;}
-            
-            return true;
+class Solution {
+  public:
+    vector<int> nextPalindrome(vector<int>& num) {
+        // code here
+        int n=num.size();
+        bool is9 = true;
+        for(auto& x:num){
+            if(x!=9){
+                is9 = false;
+            }
+        }
+        if(is9){
+            vector<int> res(n+1, 0);
+            res[0]=1;
+            res[n]=1;
+            return res;
+        }
         
+        vector<int> ans = num;
+        int l=0; 
+        int r=n-1;
+        int mid = l+(r-l)/2;
+        if(n%2!=0){ //odd
+            l = mid-1;
+            r = mid+1;
+            bool inc = false;
+            bool same = false;
+            while(r<n && l>=0){
+                if(num[l]>num[r]){
+                    inc = true;
+                    break;
+                }
+                else if(num[l]<num[r]){
+                    inc = false;
+                    break;
+                }
+                else{
+                    r++;
+                    l--;
+                }
+            }
+            if(r==n){
+                same = true;
+            }
+            
+            l=0;
+            r=n-1;
+            while(l<mid){
+                ans[r] = ans[l];
+                r--;
+                l++;
+            }
+            
+            l=mid;
+            r=mid;
+            if(!inc || same){
+                if(ans[mid]==9){
+                    while(l>=0 && r<n && ans[l]==9 && ans[r]==9){
+                        ans[l]=0;
+                        ans[r]=0;
+                        l--;
+                        r++;
+                    }
+                    if(l>=0 && r<n){
+                        ans[l]++;
+                        ans[r]++;
+                    }
+                }
+                else{
+                    ans[mid]++;
+                    return ans;
+                }
+            }
+        }
+        else{
+            // mid = mid-1;
+            l=mid-1;
+            r=mid+2;
+            bool inc = false;
+            bool same = false;
+            while(l>=0 && r<n){
+                if(num[l]>num[r]){
+                    inc = true;
+                    break;
+                }
+                else if(num[l]<num[r]){
+                    inc = false;
+                    break;
+                }
+                else{
+                    r++;
+                    l--;
+                }
+            }
+            if(r==n){
+                same = true;
+            }
+            
+            l=0;
+            r=n-1;
+            while(l<mid){
+                ans[r] = ans[l];
+                r--;
+                l++;
+            }
+            
+            l=mid;
+            r=mid+1;
+            if(ans[mid]!=ans[mid+1]){
+                if(ans[mid]>ans[mid+1]){
+                    ans[mid+1] = ans[mid];
+                }
+                else{
+                    ans[mid]++;
+                    ans[mid+1]=ans[mid];
+                }
+                return ans;
+            }
+            else if(!inc || same){
+                if(ans[mid]==9){
+                    while(l>=0 && r<n && ans[l]==9 && ans[r]==9){
+                        ans[l]=0;
+                        ans[r]=0;
+                        l--;
+                        r++;
+                    }
+                    if(l>=0 && r<n){
+                        ans[l]++;
+                        ans[r]++;
+                    }
+                }
+                else{
+                    ans[mid]++;
+                    ans[mid+1]++;
+                    return ans;
+                }
+            }
+        }
+        
+        
+        return ans;
     }
-    
-    void Nextpalindromeunntil(int num[], int n){
-        int mid = n/2;
-        
-        bool leftSmaller = false;
-        
-        int i = mid -1;
-        int j = (n % 2 == 0) ? mid : mid + 1;
-        
-        while(i>=0 && num[i]==num[j])
-        {
-            i--;
-            j++;
-        }
-        
-        if(i<0 || num[i]<num[j])
-        leftSmaller = true;
-        
-        while(i >= 0){
-            num[j] = num[i];
-            j++;
-            i--;
-        }
-        
-        if(leftSmaller == true){
-            int carry = 1;
-            
-            i = mid-1;
-            if(n%2==1){
-                num[mid] += carry;
-                carry = num[mid]/10;
-                num[mid]%=10;
-                j = mid +1;
-            }
-            
-            else{
-                j = mid;
-            }
-            
-            while (i >= 0 && carry > 0) {  // ✅ Correct condition
-                num[i] += carry;
-                carry = num[i] / 10;
-                num[i] %= 10;
-                num[j++] = num[i--];  // ✅ Correct position for j++
-            }
-        }
-        
-    }
-    
-	vector<int> generateNextPalindrome(int num[], int n) {
-	    // code here
-	    vector<int>ans;
-	   if (isallnine(num, n)) {
-            ans.push_back(1);
-            for (int i = 0; i < n - 1; i++) { // ✅ Fix: n-1 instead of n
-                ans.push_back(0);
-            }
-            ans.push_back(1);
-        return ans;  // ✅ Immediately return to avoid further processing
-        }
-
-	    
-	    else{
-	        Nextpalindromeunntil(num,n);
-	        
-	        for(int i=0; i<n; i++){
-	            ans.push_back(num[i]);
-	        }
-	    }
-	    
-	    return ans;
-	    
-	}
-
 };
-
-
-//{ Driver Code Starts.
-
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        int num[n];
-        for (int i = 0; i < n; i++) {
-            cin >> num[i];
-        }
-        Solution ob;
-        auto ans = ob.generateNextPalindrome(num, n);
-        for (auto x : ans) {
-            cout << x << " ";
-        }
-        cout << "\n";
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-
-// } Driver Code Ends
